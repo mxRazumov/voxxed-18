fa3dbd9e600b281c3cfc28bfbb49130364986b22
Derek Troy-West <derek@troywest.com>
2018-05-02 00:16:59 +1000

    dependencies

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.4.474"]
                  [org.clojure/tools.logging "0.4.0"]
+
                  [com.troy-west/arche-async "0.4.2"]
                  [com.troy-west/arche-hugcql "0.4.2" :exclusions [org.clojure/tools.reader]]
                  [com.troy-west/arche-integrant "0.4.2"]
                  [com.troy-west/thimble-all "0.1.9"]
+
                  [clj-time "0.14.3"]
-                 [cc.qbits/alia-joda-time "4.1.1" :exclusions [joda-time]]
                  [metrics-clojure "2.10.0" :exclusions [org.clojure/clojure]]
-                 [metrics-clojure-jvm "2.10.0" :exclusions [org.clojure/clojure]]
                  [metrics-clojure-riemann "2.10.0" :exclusions [org.clojure/clojure]]
                  [ch.qos.logback/logback-classic "1.2.3"]])
