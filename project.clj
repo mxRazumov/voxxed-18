3d613d735ae95142d0591bbd84210d7151282e33
Derek Troy-West <derek@troywest.com>
2018-05-01 23:59:43 +1000

    project.clj and logging defaults

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject com.troy-west/voxxed-days-18 "0.1.0-SNAPSHOT"
+
+  :description "Streaming Data Platforms & Clojure Live Demo"
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.troy-west/arche-async "0.4.2"]
+                 [com.troy-west/arche-hugcql "0.4.2"]
+                 [com.troy-west/arche-integrant "0.4.2"]
+                 [com.troy-west/thimble-all "0.1.9"]
+                 [ch.qos.logback/logback-classic "1.2.3"]])
